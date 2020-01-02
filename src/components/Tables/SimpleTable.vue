854def770149022c236791b8fb0e935482e4922a
Nesrine <37253758+Nesrineheb@users.noreply.github.com>
2020-01-02 15:16:24 +0100

    affichernoteetud

--- src/components/Tables/SimpleTable.vue
+++ src/components/Tables/SimpleTable.vue
@@ -4,11 +4,14 @@
     <md-table v-model="table_notes" :table-header-color="tableHeaderColor">
       <md-table-row slot="md-table-row" slot-scope="{ item }">
         <!--ici c'est les colonnes-->
-        <md-table-cell md-label="Module">{{ item.module }}</md-table-cell>
-        <md-table-cell md-label="TD">{{ item.TD }}</md-table-cell>
-        <md-table-cell md-label="TP">{{ item.TP }}</md-table-cell>
+        <md-table-cell md-label="AScol">{{ item.AScol }}</md-table-cell>
+        <md-table-cell md-label="MatrEtud">{{ item.MatrEtud }}</md-table-cell>
+         <md-table-cell md-label="CodeMod">{{ item.CodeMod }}</md-table-cell>
+        <md-table-cell md-label="CC">{{ item.CC }}</md-table-cell>
         <md-table-cell md-label="CI">{{ item.CI }}</md-table-cell>
-        <md-table-cell md-label="EMD">{{ item.EMD }}</md-table-cell>
+        <md-table-cell md-label="TP">{{ item.TP }}</md-table-cell>
+        <md-table-cell md-label="CF">{{ item.CF }}</md-table-cell>
+        <md-table-cell md-label="Moy">{{ item.Moy }}</md-table-cell>
       </md-table-row>
     </md-table>
     
@@ -82,8 +85,9 @@ submitReclam() {
 },
       mounted () {
     axios
-      .get('hi nesrine , met ici le lien de ton api')
+      .get('http://127.0.0.1:8000/api/serache')
       .then((response) => {
+                   
 
                          this.table_notes = response;
                         console.log(response);// vérifie dans ta console ce que reponse qui est ta table du back affiche ! 
